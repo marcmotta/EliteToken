32b9182b5cc3464e6b5d1fb31ec8f71489b10096
Marcos Mota <60157172+marcmotta@users.noreply.github.com>
2025-07-27 11:03:27 +0700

    Add elitetoken.go
    Co-authored-by: Hajigur <66867581+hajigur69@users.noreply.github.com>

---
+++ internal/elitetoken/elitetoken.go
@@ -0,0 +1,36 @@
+// internal/elitetoken/elitetoken.go
+package elitetoken
+
+import (
+	"log"
+	"os"
+)
+
+type App struct {
+	verbose bool
+	logger  *log.Logger
+}
+
+func NewApp(verbose bool) *App {
+	app := &App{
+		verbose: verbose,
+		logger:  log.New(os.Stdout, "", log.LstdFlags),
+	}
+
+	if verbose {
+		app.logger.SetPrefix("[DEBUG] ")
+	} else {
+		app.logger.SetPrefix("[INFO] ")
+	}
+
+	return app
+}
+
+func (a *App) Run() error {
+	a.logger.Printf("Starting EliteToken processing")
+
+	// Add your implementation here
+
+	a.logger.Println("Processing completed successfully")
+	return nil
+}
